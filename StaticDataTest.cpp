437a9c880f4c3ee2d221a09cbc9559cd383d0582
Ron Weber <rdw62@case.edu>
2017-03-09 21:56:48 -0500

    Tests

--- StaticDataTest.cpp
+++ StaticDataTest.cpp
@@ -1,5 +1,20 @@
 #include "catch.hpp"
+#include "GlobalIncludes.hpp"
 #include "Game.hpp"
+#include "AbilityType.hpp"
+
+TEST_CASE("Test ability.") {
+	string test = "{\
+      \"id\": \"selfPunch\",
+      \"description\": \"Punches self to make room\",
+      \"imageFile\": \"selfPunch.png\",
+      \"selection\": false,
+      \"availableFn\": \"selfPunchAvailable\",
+      \"actionFn\": \"selfPunchAction\",
+      \"helperDataFn\": \"selfPunchHelperData\"
+    }";
+	AbilityType 
 
 TEST_CASE("A game is loaded from the predetermined file.") {
-	REQUIRE_NOTHROW(Game testGame("games/
+	Game testGame("games/testgame/game.json");
+}
