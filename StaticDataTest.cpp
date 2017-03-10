658b7c8efd9e9f168a743842bf849c05d6898646
Ron Weber <rdw62@case.edu>
2017-03-09 22:02:02 -0500

    More tests.

--- StaticDataTest.cpp
+++ StaticDataTest.cpp
@@ -4,16 +4,17 @@
 #include "AbilityType.hpp"
 
 TEST_CASE("Test ability.") {
-	string test = "{\
-      \"id\": \"selfPunch\",
-      \"description\": \"Punches self to make room\",
-      \"imageFile\": \"selfPunch.png\",
-      \"selection\": false,
-      \"availableFn\": \"selfPunchAvailable\",
-      \"actionFn\": \"selfPunchAction\",
-      \"helperDataFn\": \"selfPunchHelperData\"
-    }";
-	AbilityType 
+	json test = "{\
+      \"id\": \"selfPunch\",\
+      \"description\": \"Punches self to make room\",\
+      \"imageFile\": \"selfPunch.png\",\
+      \"selection\": false,\
+      \"availableFn\": \"selfPunchAvailable\",\
+      \"actionFn\": \"selfPunchAction\",\
+      \"helperDataFn\": \"selfPunchHelperData\"\
+    }"_json;
+	AbilityType type = AbilityType(test);
+}
 
 TEST_CASE("A game is loaded from the predetermined file.") {
 	Game testGame("games/testgame/game.json");
